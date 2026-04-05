518d9283093e4e342e8cc9c8e0df8303229d2196
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2026-04-05 18:53:22 +0100

    sixth commit

--- api/comentarios.js
+++ api/comentarios.js
@@ -1,23 +1,21 @@
-import { sql } from '@vercel/postgres';
+import { db } from '@vercel/postgres'; // Mudamos de 'sql' para 'db'
 
 export default async function handler(request, response) {
-  // 1. Rota para SALVAR (POST)
+  const client = await db.connect(); // Isto força a ligação usando as variáveis do painel
+
   if (request.method === 'POST') {
     const { texto } = request.body;
-    
     try {
-      // O comando SQL puro que aprendemos
-      await sql`INSERT INTO comentarios (texto) VALUES (${texto});`;
+      await client.sql`INSERT INTO comentarios (texto) VALUES (${texto});`;
       return response.status(200).json({ mensagem: "Sucesso!" });
     } catch (error) {
       return response.status(500).json({ error: error.message });
     }
   }
 
-  // 2. Rota para LISTAR (GET)
   if (request.method === 'GET') {
     try {
-      const { rows } = await sql`SELECT * FROM comentarios ORDER BY id DESC;`;
+      const { rows } = await client.sql`SELECT * FROM comentarios ORDER BY id DESC;`;
       return response.status(200).json(rows);
     } catch (error) {
       return response.status(500).json({ error: error.message });
